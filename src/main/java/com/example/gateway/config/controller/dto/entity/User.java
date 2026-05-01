a261809a4f1841c1bb26815d0577ab153359b0b4
Ashwin Krishna Vijay <internetpasanga3@gmail.com>
2026-05-01 16:15:56 +0530

    Add User entity class for MongoDB

---
+++ src/main/java/com/example/gateway/config/controller/dto/entity/User.java
@@ -0,0 +1,19 @@
+package com.example.gateway.entity;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    private String id;
+    private String username;
+    private String password;
+    private String role;
+}
